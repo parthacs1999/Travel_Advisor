43a4ed9320919bda61c88009194fd3c0d0aa0b2f
Partha Chakraborty <partha.c1999@gmail.com>
2023-02-03 22:39:28 +0530

    Project Structure created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import { CssBaseline, Grid } from '@material-ui/core';
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4} >
+          <List/>
+        </Grid>
+        <Grid item xs={12} md={8} >
+          <Map/>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
